c36192a48fa8322d6d731e93d61dfbf102b0c1df
rudwnlee2 <lgj0731@naver.com>
2025-11-08 23:45:47 +0900

    feat: Main 서버 실행 진입점 추가

--- src/main/java/chat/ChatApplication.java
+++ src/main/java/chat/ChatApplication.java
@@ -1,7 +1,13 @@
 package chat;
 
+import chat.server.ChatServer;
+
 public class ChatApplication {
     public static void main(String[] args) {
-        System.out.println("Chat Application is running!");
+        // 1. 우리 서버를 9090 포트로 생성합니다.
+        ChatServer chatServer = new ChatServer(9090);
+
+        // 2. 서버를 실행시킵니다.
+        chatServer.start();
     }
 }
